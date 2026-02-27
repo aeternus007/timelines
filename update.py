1c5c56b2ec944e30f64b0327a07bd2a41ce79f9c
Ray Libbenga <rjlibbenga@hotmail.com>
2026-02-27 17:45:35 +0000

    first version of the update mechanism

--- update.py
+++ update.py
@@ -1,43 +1,30 @@
 #!/usr/bin/env python3
 import glob
+from json import loads
 from os import path, remove
-import re
 from requests import get
 
-from bs4 import BeautifulSoup as bs
+base_path = path.split(__file__)[0]
+version = glob.glob(path.join(base_path, "version_*"))[0].split("_")[-1]
 
-base_url = "https://github.com/aeternus007/timelines/"
-sub_url = base_url + "https://github.com/aeternus007/timelines/tree/main/src"
+files = loads(get("https://raw.githubusercontent.com/aeternus007/timelines/refs/heads/main/files.json").text)
+version_online = files[-1].split("_")[-1]
 
-base_raw_url = "https://raw.githubusercontent.com/aeternus007/timelines/refs/heads/main/"
-sub_raw_url = base_raw_url + "src/"
-
-version_pattern = re.compile(r"version_[0-9]+\.[0-9]*")
-file_pattern = re.compile(r"[a-zA-z0-9]+\.[a-zA-z0-9]+")
-
-page = get(sub_url, headers= {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0}"})
-print(page)
-print(page.text)
-# [filename.extract().get_text() for filename in bs(get(base_url).text, "lxml").find_all(title=file_pattern)] + ["src" + filename.extract().get_text() for filename in bs(get(sub_url).text, "lxml").find_all(title=file_pattern)]
-filenames = bs(page.text, "lxml").find_all(title=file_pattern)
-print(filenames)
-
-# def check_for_new():
-#     return
-
-# def get_file_urls():
-#     return
-
-# def filter_files(filename):
-#     if "json" in filename:
-#         return False
-#     if "update" in filename:
-#         return False
+def filter_files(filename):
+    if "json" in filename:
+        return False
+    if "update" in filename:
+        return False
     
-#     return True
+    return True
 
-# base_path = path.split(__file__)[0]
-# for file in filter(filter_files, glob.glob(path.join(base_path, "**", "*"), recursive=True)):
-#     print(file)
+if version != version_online:
+    for file in filter(filter_files, glob.glob(path.join(base_path, "**", "*"), recursive=True)):
 
-#     # remove(file)
+        remove(file)
+
+    for file in files:
+        filename = path.join(base_path, path.split(file)[-1])
+
+        with open(filename, "w") as destination:
+            destination.write(get(file).text)
