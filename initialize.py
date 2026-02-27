1d044103797e66b66e073366acc316ce07f8cf8e
Ray Libbenga <rjlibbenga@hotmail.com>
2026-02-27 18:05:52 +0000

    minor fixes

--- initialize.py
+++ initialize.py
@@ -7,6 +7,6 @@ run(f"python -m pip install -r {path.join(base_path, "requirements.txt")}")
 
 with open(path.join(base_path, "startup.cmd"), "w") as startup:
     startup.truncate(0)
-    startup.write(r"start python %CD%\update.py && start python %CD%\src\server.py && start python %CD%\src\client.py")
+    startup.write(r"start python %~dp0update.py && start python %~dp0src\server.py && start python %~dp0src\client.py")
 
 remove(__file__)
--- update.py
+++ update.py
@@ -7,7 +7,8 @@ from requests import get
 base_path = path.split(__file__)[0]
 version = glob.glob(path.join(base_path, "version_*"))[0].split("_")[-1]
 
-files = loads(get("https://raw.githubusercontent.com/aeternus007/timelines/refs/heads/main/files.json").text)
+response = get("https://raw.githubusercontent.com/aeternus007/timelines/refs/heads/main/files.json")
+files = loads(response.text)
 version_online = files[-1].split("_")[-1]
 
 def filter_files(filename):
@@ -20,11 +21,13 @@ def filter_files(filename):
 
 if version != version_online:
     for file in filter(filter_files, glob.glob(path.join(base_path, "**", "*"), recursive=True)):
-
         remove(file)
+        print(f"{file} is deleted")
 
     for file in files:
         filename = path.join(base_path, path.split(file)[-1])
+        print(f"{filename} is downloading")
 
         with open(filename, "w") as destination:
             destination.write(get(file).text)
+            print(f"{destination} is done")
